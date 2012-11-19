ecfadd2622e921ca91c268ab0b3b93abc6f0d39b
George Opritescu <ssscripting@gmail.com>
2012-11-19 12:22:34 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Answerbox::Application.config.secret_token = '8fee06bf91cda8f755ba7028cda2af51d43b7f852694a96b9f5937c6191deae3fc9e1c68a5d66851a8b048fcd004817405094bdd1a9b19ac7c1582e6bd37efd7'
