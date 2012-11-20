ab60ad5fe6c4b57e5c360a2d2f2bdee1f0921742
Cosmin Barbu <cosmin.barbu@tutoria.de>
2012-11-20 09:52:48 +0200

    #master - markdown for answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -11,8 +11,18 @@ class Answer < ActiveRecord::Base
     self.save
   end
 
+  before_save :render_body
+
   private
+  def render_body
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    self.body = redcarpet.render(self.body)
+  end
+
   def question_answered
     errors[:answered] << 'Question releated to this answer is already answered' if self.question.answered?
   end
+
 end
