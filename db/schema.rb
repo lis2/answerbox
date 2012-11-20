5e9942bf3b37b8671717735e8f13fc568c52425c
Krzysztof Kotlarek <kotlarek.krzysztof@gmail.com>
2012-11-20 13:06:44 +0200

    #master - start with points

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120082310) do
+ActiveRecord::Schema.define(:version => 20121120105001) do
 
   create_table "answers", :force => true do |t|
     t.integer  "user_id"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(:version => 20121120082310) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.integer  "points"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
