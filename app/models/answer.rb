3fe642435d3a2c134ad2de96bf63bb0ecbfa7278
George Opritescu <ssscripting@gmail.com>
2012-11-20 13:06:27 +0200

    Added validation so that a user may only add one answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,12 @@
 class Answer < ActiveRecord::Base
   attr_accessible :answered, :body, :question_id, :user_id
+
   belongs_to :question
   belongs_to :user
   
   validates :body, presence: true, length: { minimum: 1 }
   validate :question_answered
+  validate :only_one_answer_per_user_per_question
  
   def mark_as_answered!
     self.answered = true
@@ -19,6 +21,12 @@ class Answer < ActiveRecord::Base
     extensions = {fenced_code_blocks: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     self.body = redcarpet.render(self.body)
+  end
+
+  def only_one_answer_per_user_per_question
+    question = self.question
+    already_answered_question = question.answers.find {|e| e.user == self.user }
+    errors[:answered] << "You have already answered that question" if already_answered_question
   end
 
   def question_answered
