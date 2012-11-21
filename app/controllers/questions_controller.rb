71d876bb6cfb0cc44c9623b1c5da972a2b442fe5
George Opritescu <ssscripting@gmail.com>
2012-11-21 14:33:39 +0200

    After merge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_filter :authenticate_user!, except: [:index,:show]
+  before_filter :authenticate_user!, except: [:index,:show,:filter]
 
   def index
     @questions = Question.paginate(:page => params[:page],:per_page => 10)
